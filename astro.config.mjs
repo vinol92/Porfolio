e317cfc909702fe6f49b1355ebc5978912a27ddf
vinol92 <vinol92@gmail.com>
2024-05-14 13:34:26 +0200

    porfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindConfig from './tailwind.config.mjs';
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  output: "server",
+  adapter: vercel()
 });
